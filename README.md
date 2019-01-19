287f8685af90e0bbdec04c082abc6ad273e3f03f
jojo2454 <mingming12369852@gmail.com>
2019-01-19 21:22:36 +0800

    新增鍵盤控制

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Html-in_Game-tool
-這是一款 協助html 開發的工具
-目前：規劃開發中
+這是一款 協助html遊戲 開發的工具
+
+how to installation
+
+```
+<script src="js/all.js" type="text/javascript">
+
+確認是否安裝成功
+Detection.hello();
+>>>hello!!
+
+啟用　檢測鍵盤腳本
+<body onload="Detection.start_key()">
+檢測訊息在"F12"中..
+
+
+
+
+
+```
+目前：規劃開發中<br>
+[x]鍵盤輸入　<br>
+  [ ]玩家控制<br>
+[ ]滑鼠輸入<br>
+[ ]玩家碰撞事件<br>
+[ ]怪物碰撞事件<br>
+[ ]重力<br>
---
+++ js/all.js
@@ -0,0 +1,66 @@
+var Detection = {
+  hello: function() {
+    console.log("hello!!");
+  },
+
+  collision: function(a) {
+    // 檢測用工具
+    // console.log(event.keyCode);
+    switch (event.keyCode) {
+      case 37:
+        key_left();
+        break;
+      case 38:
+        key_top();
+        break;
+        case 39:
+        key_right();
+        break;
+        case 40:
+        key_under();
+        break;
+      default:
+      console.log("請使用鍵盤上的　上下左右　遊戲");
+
+    }
+
+    function key_left() {
+      console.log("this ledt");
+    }
+    function key_top() {
+      console.log("this top");
+    }
+    function key_right() {
+      console.log("this right");
+    }
+    function key_under() {
+      console.log("this under");
+    }
+  },
+
+
+  start_key: function() {
+
+    function keyFunction(a) {
+      Detection.collision(a);
+
+    }
+    document.onkeydown = keyFunction;
+  }
+
+
+
+}
+
+
+
+//鍵盤事件
+
+
+
+
+//event.keyCode
+// 37左
+// 38上
+// 39右
+// 40下
